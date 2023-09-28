19b00823efaf64a8a7d8d7e4b038729ba6613771
Neha <sniravj@gmail.com>
2023-09-28 15:15:58 +0530

    Started

---
+++ string.py
@@ -0,0 +1,31 @@
+email = """
+string email
+abc@email.com
+"""
+print(email)
+
+a = " hello world Hello Testing "
+print(a[3:])
+print(a[:10])
+print(len(a))
+print(a.upper())
+print(a.capitalize())
+
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+
+print(a.replace("Testing" ,"TestTest"))
+print(a.replace(" ",""))
+print(a.find("Hello"))
+list = a.split(" ")
+print(list)
+
+a = "hello"
+b = "world"
+
+if a == b:
+    print("equal")
+else:
+    print("not equal")
+
